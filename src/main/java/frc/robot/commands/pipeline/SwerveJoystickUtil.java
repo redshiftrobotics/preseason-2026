8c111cf03fce6ac56173d70d4ecebbdd17fccb24
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2025-08-31 02:54:00 +0100

    Clean up SwerveJoystickUtil

--- src/main/java/frc/robot/commands/pipeline/SwerveJoystickUtil.java
+++ src/main/java/frc/robot/commands/pipeline/SwerveJoystickUtil.java
@@ -1,32 +1,34 @@
 package frc.robot.commands.pipeline;
 
-import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.utility.JoystickUtil;
 
 public class SwerveJoystickUtil {
+
+  public static final double JOYSTICK_DEADBAND = 0.15;
+  public static final double ANGLE_DEADBAND = 0.5;
+
+  public static final double LINEAR_VELOCITY_POWER = 2.0;
+  public static final double ANGULAR_VELOCITY_POWER = 2.0;
+
   public static Translation2d getTranslationMetersPerSecond(
       double xInput, double yInput, double maxTranslationSpeedMetersPerSecond) {
 
     Translation2d translation = new Translation2d(xInput, yInput);
 
     // get length of linear velocity vector, and apply deadband to it for noise reduction
-    double magnitude = JoystickUtil.applyDeadband(translation.getNorm());
+    double magnitude = MathUtil.applyDeadband(translation.getNorm(), JOYSTICK_DEADBAND);
 
-    if (magnitude == 0) return new Translation2d();
+    if (magnitude == 0) return Translation2d.kZero;
 
     // squaring the magnitude of the vector makes for quicker ramp up and slower fine control,
     // magnitude should always be positive
-    double magnitudeSquared = Math.copySign(Math.pow(magnitude, 2), 1);
+    double magnitudeSquared = Math.abs(Math.pow(magnitude, LINEAR_VELOCITY_POWER));
 
     // get a vector with the same angle as the base linear velocity vector but with the
     // magnitude squared
-    Translation2d squaredLinearVelocity =
-        new Pose2d(new Translation2d(), translation.getAngle())
-            .transformBy(new Transform2d(magnitudeSquared, 0.0, new Rotation2d()))
-            .getTranslation();
+    Translation2d squaredLinearVelocity = new Translation2d(magnitudeSquared, translation.getAngle());
 
     // return final value
     return squaredLinearVelocity.times(maxTranslationSpeedMetersPerSecond);
@@ -36,11 +38,11 @@ public class SwerveJoystickUtil {
       double omegaInput, double maxAngularSpeedRadPerSec) {
 
     // get rotation speed, and apply deadband
-    double omega = JoystickUtil.applyDeadband(omegaInput);
+    double omega = MathUtil.applyDeadband(omegaInput, JOYSTICK_DEADBAND);
 
     // square the omega value for quicker ramp up and slower fine control
     // make sure to copy the sign over for direction
-    double omegaSquared = Math.copySign(Math.pow(omega, 2), omega);
+    double omegaSquared = Math.copySign(Math.pow(omega, ANGULAR_VELOCITY_POWER), omega);
 
     // return final value
     return omegaSquared * maxAngularSpeedRadPerSec;
@@ -50,11 +52,11 @@ public class SwerveJoystickUtil {
     // Get desired angle as a vector
     final Translation2d desiredAngle = new Translation2d(xInput, yInput);
 
-    // If the vector length is longer then our deadband update the heading controller
-    if (desiredAngle.getNorm() > 0.5) {
-      return desiredAngle.getAngle();
+    // If the vector length is less than the deadband, return null
+    if (desiredAngle.getNorm() < ANGLE_DEADBAND) {
+      return null;
     }
 
-    return null;
+    return desiredAngle.getAngle();
   }
 }
