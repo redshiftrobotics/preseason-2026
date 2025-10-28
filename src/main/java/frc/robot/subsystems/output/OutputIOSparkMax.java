ea77fede24a2cd3f035004fa13420834878108f0
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2025-10-28 15:49:03 -0700

    Finish initial version of output subsystem Spark MAX IO layer

--- src/main/java/frc/robot/subsystems/output/OutputIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/output/OutputIOSparkMax.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.output;
 
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.util.Units;
 
 public class OutputIOSparkMax implements OutputIO {
@@ -9,6 +12,9 @@ public class OutputIOSparkMax implements OutputIO {
 
   public OutputIOSparkMax(int deviceID) {
     motor = new SparkMax(deviceID, MotorType.kBrushless);
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.idleMode(IdleMode.kBrake);
+    motor.configure(config, null, null);
   }
 
   /** Updates the set of loggable inputs. */
@@ -18,11 +24,22 @@ public class OutputIOSparkMax implements OutputIO {
   }
 
   /** Sets the velocity of the motor */
-  public void setVelocity(double velocityRadPerSec) {}
+  public void setVelocity(double velocityRadPerSec) {
+    motor
+        .getClosedLoopController()
+        .setReference(
+            Units.radiansPerSecondToRotationsPerMinute(velocityRadPerSec), ControlType.kVelocity);
+  }
 
   /** Stop the motor and enable the brake */
-  public void stop() {}
+  public void stop() {
+    motor.stopMotor();
+  }
 
   /** Configure the PID constants */
-  public void configurePID(double kP, double kI, double kD) {}
+  public void configurePID(double kP, double kI, double kD) {
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.closedLoop.pid(kP, kI, kD);
+    motor.configure(config, null, null);
+  }
 }
