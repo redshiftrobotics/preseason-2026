55fe9f8797168bd41e5fc3e96c0f66d19a6767a3
MichaelLesirge <michael.lesirge@gmail.com>
2025-11-02 23:04:02 -0800

    fix formatting

--- src/main/java/frc/robot/utility/AllianceFlipUtil.java
+++ src/main/java/frc/robot/utility/AllianceFlipUtil.java
@@ -1,7 +1,6 @@
 package frc.robot.utility;
 
 import com.pathplanner.lib.util.FlippingUtil;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
